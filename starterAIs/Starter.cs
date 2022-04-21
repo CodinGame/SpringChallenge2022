7a0c4190a82fa54196fa7a7e7fa17ca1ff3f0ead
ShadyNagy <info@shadynagy.com>
2022-04-21 16:57:41 +0200

    csharp added.

---
+++ starterAIs/Starter.cs
@@ -0,0 +1,124 @@
+using System;
+using System.Collections.Generic;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Player
+{
+    public const int TYPE_MONSTER = 0;
+    public const int TYPE_MY_HERO = 1;
+    public const int TYPE_OP_HERO = 2;
+
+    public class Entity
+    {
+        public int Id;
+        public int Type;
+        public int X, Y;
+        public int ShieldLife;
+        public int IsControlled;
+        public int Health;
+        public int Vx, Vy;
+        public int NearBase;
+        public int ThreatFor;
+
+        public Entity(int id, int type, int x, int y, int shieldLife, int isControlled, int health, int vx, int vy, int nearBase, int threatFor)
+        {
+            this.Id = id;
+            this.Type = type;
+            this.X = x;
+            this.Y = y;
+            this.ShieldLife = shieldLife;
+            this.IsControlled = isControlled;
+            this.Health = health;
+            this.Vx = vx;
+            this.Vy = vy;
+            this.NearBase = nearBase;
+            this.ThreatFor = threatFor;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string[] inputs;
+        inputs = Console.ReadLine().Split(' ');
+
+        // base_x,base_y: The corner of the map representing your base
+        int baseX = int.Parse(inputs[0]);
+        int baseY = int.Parse(inputs[1]);
+
+        // heroesPerPlayer: Always 3
+        int heroesPerPlayer = int.Parse(Console.ReadLine());
+
+        // game loop
+        while (true)
+        {
+
+            inputs = Console.ReadLine().Split(' ');
+            int myHealth = int.Parse(inputs[0]); // Your base health
+            int myMana = int.Parse(inputs[1]); // Ignore in the first league; Spend ten mana to cast a spell
+
+            inputs = Console.ReadLine().Split(' ');
+            int oppHealth = int.Parse(inputs[0]);
+            int oppMana = int.Parse(inputs[1]);
+
+            int entityCount = int.Parse(Console.ReadLine()); // Amount of heros and monsters you can see
+
+            List<Entity> myHeroes = new List<Entity>(entityCount);
+            List<Entity> oppHeroes = new List<Entity>(entityCount);
+            List<Entity> monsters = new List<Entity>(entityCount);
+
+            for (int i = 0; i < entityCount; i++)
+            {
+                inputs = Console.ReadLine().Split(' ');
+                int id = int.Parse(inputs[0]); // Unique identifier
+                int type = int.Parse(inputs[1]); // 0=monster, 1=your hero, 2=opponent hero
+                int x = int.Parse(inputs[2]); // Position of this entity
+                int y = int.Parse(inputs[3]);
+                int shieldLife = int.Parse(inputs[4]); // Ignore for this league; Count down until shield spell fades
+                int isControlled = int.Parse(inputs[5]); // Ignore for this league; Equals 1 when this entity is under a control spell
+                int health = int.Parse(inputs[6]); // Remaining health of this monster
+                int vx = int.Parse(inputs[7]); // Trajectory of this monster
+                int vy = int.Parse(inputs[8]);
+                int nearBase = int.Parse(inputs[9]); // 0=monster with no target yet, 1=monster targeting a base
+                int threatFor = int.Parse(inputs[10]); // Given this monster's trajectory, is it a threat to 1=your base, 2=your opponent's base, 0=neither
+
+                Entity entity = new Entity(
+                    id, type, x, y, shieldLife, isControlled, health, vx, vy, nearBase, threatFor
+                );
+
+                switch (type)
+                {
+                    case TYPE_MONSTER:
+                        monsters.Add(entity);
+                        break;
+                    case TYPE_MY_HERO:
+                        myHeroes.Add(entity);
+                        break;
+                    case TYPE_OP_HERO:
+                        oppHeroes.Add(entity);
+                        break;
+                }
+            }
+            for (int i = 0; i < heroesPerPlayer; i++)
+            {
+                Entity target = null;
+
+                if (monsters.Count > 0)
+                {
+                    target = monsters[i % monsters.Count];
+                }
+
+                if (target != null)
+                {
+                    Console.WriteLine($"MOVE {target.X} {target.Y}");
+                }
+                else
+                {
+                    Console.WriteLine("WAIT");
+                }
+            }
+        }
+    }
+}
